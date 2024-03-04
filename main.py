ba2879cc33c78033f568f40d25c29b2a52e42e27
Daniel Hinton <djhinton96@outlook.com>
2024-03-04 18:34:49 +1100

    Created deck class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Card data
 
 suits = ('♠', '♥', '♦', '♣')
@@ -8,6 +9,7 @@ ranks = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
 
 values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10,
           'J':10, 'Q':10, 'K':10, 'A':11}    # Make A 1 or 11
+
 
 # Card class
 
@@ -21,3 +23,26 @@ class Card:
     def __str__(self):
         return f'{self.rank}{self.suit}'
     
+
+# Deck class - Create a deck of cards with functions to shuffle and deal one.
+# Takes single int argument for number of decks (Blackjack can be played with
+# 1-8 decks depending on the game rules)
+
+class Deck:
+
+    def __init__(self, num_of_decks):
+        self.all_cards = []
+        for i in range(num_of_decks):
+            for suit in suits:
+                for rank in ranks:
+                    self.all_cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+    
+    def deal_a_card(self):
+        return self.all_cards.pop()
+
+card_shoe = Deck(8)
+card_shoe.shuffle()
+print(len(card_shoe.all_cards))
